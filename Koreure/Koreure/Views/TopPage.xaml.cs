e442119616a9cff845248dfaae686ad812a75a30
偏差値24 <t.29.wuq@gmail.com>
2018-08-03 15:54:36 +0900

    first

---
+++ Koreure/Koreure/Views/TopPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Koreure.Views
+{
+    public partial class TopPage : CarouselPage
+    {
+        public TopPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
